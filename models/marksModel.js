da8028e366bba8a1d2e01a7187dcb6f004fb7a3c
nir presser <nirpresser@gmail.com>
2021-08-30 23:34:53 +0300

    Can add now new grades to mongoDB

--- models/marksModel.js
+++ models/marksModel.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const MarksSchema = new mongoose.Schema({
-    ID: { type: Number, required: true },
-    name: { type: String, required: false, default: 'UnKnown' },
-    Algebra: { type: Number, min: 0, max: 100, required: true},
-    Infinite: { type: Number, min: 0, max: 100, required: true},
-    Discrete: { type: Number, min: 0, max: 100, required: true},
-    Python: { type: Number, min: 0, max: 100, required: true},
-    Final: { type: Number, min: 0, max: 100, required: true}
+    ID: { type: Number},
+    name: { type: String, default: 'UnKnown' },
+    Algebra: { type: Number, min: 0, max: 100},
+    Infinite: { type: Number, min: 0, max: 100},
+    Discrete: { type: Number, min: 0, max: 100},
+    Python: { type: Number, min: 0, max: 100},
+    Final: { type: Number, min: 0, max: 100}
 });
 
 module.exports = mongoose.model('MarksSchema', MarksSchema);
