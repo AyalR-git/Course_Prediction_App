ce400c99251b4bc5ca51e916d948c1502917a76a
Gal R <galrn6@gmail.com>
2021-09-10 00:39:29 +0300

    testing worflow test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
-	useUnifiedTopology: true
+    useUnifiedTopology: true
 });
 console.log('Connected to mongoDB.');
 
