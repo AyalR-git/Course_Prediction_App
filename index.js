8e34b03aa2a41d3c6ef6099816601dddc7e480ad
Gal R <galrn6@gmail.com>
2021-09-10 09:41:44 +0300

    added staticRoutes

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ mongoose.connect(process.env.MONGO_URI, {
 console.log('Connected to mongoDB.');
 
 app.use('/static', express.static(path.join(__dirname, 'data')));
+app.use('/staticRoutes', express.static(path.join(__dirname, 'routes')));
 
-const pagesRoutes = require('./routes/pages_routes');
+const pagesRoutes = require('staticRoutes/pages_routes');
 app.use('/', pagesRoutes);
 
 const dbRoutes = require('./routes/db_routes');
