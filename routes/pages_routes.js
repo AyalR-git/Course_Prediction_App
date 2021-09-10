be206ad96d7c1c052092b2d28dae85bc564b4638
Gal R <galrn6@gmail.com>
2021-09-10 13:34:59 +0300

    changed paths to relative

--- routes/pages_routes.js
+++ routes/pages_routes.js
@@ -6,16 +6,16 @@ router.get('/homepage', (req, res) =>
 	res.sendFile(path.join(__dirname, '../data', 'pages', 'homepage.html')));
 
 router.get('/prediction', (req, res) =>
-	res.sendFile(path.resolve('data/Pages/prediction.html')));
+	res.sendFile(path.resolve(__dirname, '../data', 'pages', 'prediction.html')));
 
 router.get('/menu', (req, res) =>
-	res.sendFile(path.resolve('data/Pages/menu.html')));
+	res.sendFile(path.resolve(__dirname, '../data', 'pages', 'menu.html')));
 
 router.get('/contribute_grades', (req, res) =>
-	res.sendFile(path.resolve('data/Pages/contribute_to_db.html')));
+	res.sendFile(path.resolve(__dirname, '../data', 'pages', 'contribute_to_db.html')));
 
 router.get('/terms_and_condition', (req, res) =>
-	res.sendFile(path.resolve('data/Pages/terms_and_condition.html')));
+	res.sendFile(path.resolve(__dirname, '../data', 'pages', 'terms_and_condition.html')));
     
 router.get('/', (req, res) =>
 	res.sendFile(path.join(__dirname, '../data', 'pages', 'homepage.html')));
